3e96cd15fa3b471992441d826dc9f826b90f830c
rajvir234567 <rajvirspurewal@gmail.com>
2020-11-28 12:11:07 +0000

    add console.log

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    console.log("git message");
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
